ebef7f8e937861c6d78a1b5723c5ea58f1e81061
myotytheking-oss <myotytheking@gmail.com>
2026-06-28 15:12:46 +0200

    configuration de fire base

--- agrismart/lib/main.dart
+++ agrismart/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 // ==========================================
 // CONFIGURATIONS ET NOTIFIERS GLOBAUX
@@ -66,7 +68,13 @@ class ResultatAnalyseIA {
   });
 }
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
